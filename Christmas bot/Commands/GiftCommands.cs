82f32d7086048591029eeecc9587ad10ed199588
someone-s <wongeden@gmail.com>
2021-11-09 19:28:24 +0800

    major code restructure

--- Christmas bot/Commands/GiftCommands.cs
+++ Christmas bot/Commands/GiftCommands.cs
@@ -1,79 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 
 namespace Christmas_bot.Commands
 {
-    public class GiftCommands : BaseCommandModule
+    public partial class GiftCommands : BaseCommandModule
     {
-        private char seperator = '^';
-
         [Command("gift-admin-add")]
+        [Aliases("gaa", "add-a")]
         [Description("add gift admin")]
-        public async Task AssignAdmin(CommandContext ctx, DiscordUser newadmin)
+        public async Task AssignAdmin(CommandContext ctx, 
+            [Description("user to add")] DiscordUser newadmin)
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
-            if (ctx.User != ctx.Guild.Owner)
+            if (!RoleHandle.IsOwner(ctx.Guild, ctx.User))
             {
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"error",
-                    Color = DiscordColor.Red,
-                    Description = $"{ctx.User.Username} is not owner"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
+                await MessageHandle.SendError(ctx.Channel,
+                    message: $"{ctx.User.Username} is not owner");
             }
             else
             {
-                var path = PathHandle.GetAdminPath(ctx.Guild.Id);
+                var path = PathHandle.GetAdminPath(ctx.Guild);
 
                 using (var sw = new StreamWriter(path, true))
                     sw.WriteLine(newadmin.Id);
 
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"success",
-                    Color = DiscordColor.Green,
-                    Description = $"{newadmin.Username} added as admin"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
+                await MessageHandle.SendSuccess(ctx.Channel,
+                    message: $"{newadmin.Username} added as admin");
             }
 
             await message.DeleteAsync();
         }
 
         [Command("gift-admin-remove")]
+        [Aliases("gar", "rm-a")]
         [Description("remove gift admin")]
-        public async Task RelegateAdmin(CommandContext ctx, DiscordUser formeradmin)
+        public async Task RelegateAdmin(CommandContext ctx, 
+            [Description("user to remove")] DiscordUser formeradmin)
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
             await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
             if (ctx.User != ctx.Guild.Owner)
-            {
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"error",
-                    Color = DiscordColor.Red,
-                    Description = $"{ctx.User.Username} is not owner"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
-            }
+                await MessageHandle.SendError(ctx.Channel,
+                    message: $"{ctx.User.Username} is not owner");
             else
             {
                 var admins = new List<ulong>();
-                var path = PathHandle.GetAdminPath(ctx.Guild.Id);
+                var path = PathHandle.GetAdminPath(ctx.Guild);
 
                 var s = string.Empty;
                 using (var fs = File.Open(path, FileMode.OpenOrCreate))
@@ -88,25 +70,21 @@ namespace Christmas_bot.Commands
                     foreach (var admin in admins)
                         sw.WriteLine(admin);
 
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"success",
-                    Color = DiscordColor.Green,
-                    Description = $"{formeradmin.Username} relegated"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
+                await MessageHandle.SendSuccess(ctx.Channel,
+                    message: $"{formeradmin.Username} relegated");
             }
 
             await message.DeleteAsync();
         }
 
         [Command("gift-admin-list")]
+        [Aliases("gal", "list-a")]
         [Description("list bot admins")]
         public async Task ListAdmin(CommandContext ctx)
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
-            var path = PathHandle.GetAdminPath(ctx.Guild.Id);
+            var path = PathHandle.GetAdminPath(ctx.Guild);
             var embed = new DiscordEmbedBuilder
             {
                 Title = $"success",
@@ -126,67 +104,70 @@ namespace Christmas_bot.Commands
 
             await message.DeleteAsync();
         }
+    }
+
+    public partial class GiftCommands
+    {
 
         [Command("gift-add")]
-        [Description("add gift to pool [type: text, url, imageurl][string]")]
-        public async Task AddGift(CommandContext ctx, string type, string gift)
+        [Aliases("ga", "add-g")]
+        [Description("add gift to pool")]
+        public async Task AddGift(CommandContext ctx,
+            [Description("either text, url, imageurl")] string type,
+            [Description("gift in text form")] string gift)
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
             var filtered = new string(gift.Where(c => c != '^').ToArray());
 
             if (filtered.Length == 0)
-            {
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"error",
-                    Color = DiscordColor.Red,
-                    Description = $"gift by {ctx.User.Username} is empty",
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
-            }
+                await MessageHandle.SendError(ctx.Channel,
+                    message: $"gift by {ctx.User.Username} is empty");
             else
             {
-                var path = PathHandle.GetGiftPath(ctx.Guild.Id);
+                var path = PathHandle.GetGiftPath(ctx.Guild);
 
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"success",
-                    Color = DiscordColor.Green,
-                };
                 using (var sw = new StreamWriter(path, true))
                 {
                     switch (type)
                     {
                         case "text":
-                            embed.Description = $"{filtered} added by {ctx.User.Username}";
-                            sw.WriteLine($"{ctx.User.Id}{seperator}text:{filtered}");
+                            await MessageHandle.SendSuccess(ctx.Channel, 
+                                message: $"{filtered} added by {ctx.User.Username}");
+                            GiftHandle.WriteGift(sw, ctx.User.Id, $"text:{filtered}");
                             break;
                         case "url":
-                            embed.Description = $"url:{filtered} added by {ctx.User.Username}";
-                            sw.WriteLine($"{ctx.User.Id}{seperator}url:{filtered}");
+                            await MessageHandle.SendSuccess(ctx.Channel,
+                                message: $"{filtered} added by {ctx.User.Username}",
+                                url: filtered);
+                            GiftHandle.WriteGift(sw, ctx.User.Id, $"url:{filtered}");
                             break;
                         case "imageurl":
-                            embed.Description = $"image:{filtered} added by {ctx.User.Username}";
-                            sw.WriteLine($"{ctx.User.Id}{seperator}image:{filtered}");
+                            await MessageHandle.SendSuccess(ctx.Channel,
+                                message: $"{filtered} added by {ctx.User.Username}",
+                                url: filtered,
+                                imageurl: filtered);
+                            GiftHandle.WriteGift(sw, ctx.User.Id, $"image:{filtered}");
                             break;
                         default:
+                            await MessageHandle.SendError(ctx.Channel,
+                                message: $"unsupported gift type {type}");
                             break;
                     }
                 }
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
             }
 
             await message.DeleteAsync();
         }
 
         [Command("gift-list")]
+        [Aliases("gl", "ls-g")]
         [Description("list gifts in pool")]
         public async Task ListGift(CommandContext ctx)
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
-            var path = PathHandle.GetGiftPath(ctx.Guild.Id);
+            var path = PathHandle.GetGiftPath(ctx.Guild);
             var embed = new DiscordEmbedBuilder
             {
                 Title = $"success",
@@ -197,11 +178,14 @@ namespace Christmas_bot.Commands
             using (var sr = new StreamReader(fs))
             {
                 var s = string.Empty;
+                var i = 1;
                 while ((s = sr.ReadLine()) != null)
                 {
-                    var p = s.Split(seperator);
-                    var sender = await ctx.Guild.GetMemberAsync(ulong.Parse(p[0])).ConfigureAwait(false);
-                    embed.AddField(p[1], sender.Username);
+                    GiftHandle.ReadGift(s, out ulong id, out string gift);
+                    var sender = await ctx.Guild.GetMemberAsync(id).ConfigureAwait(false);
+                    embed.AddField($"{i}: {gift}", sender.Username);
+
+                    i++;
                 }
             }
 
@@ -210,63 +194,62 @@ namespace Christmas_bot.Commands
             await message.DeleteAsync();
         }
 
+        [Command("gift-remove")]
+        [Aliases("gr", "rm-g")]
+        [Description("remove gift pool")]
+        public async Task RemoveGift(CommandContext ctx,
+            [Description("index of gift to be removed")] int index)
+        {
+            var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
+
+            if (!RoleHandle.IsAdmin(ctx.Guild, ctx.User))
+                await MessageHandle.SendError(ctx.Channel,
+                    message: $"{ctx.User.Username} is not admin");
+            else
+            {
+                var gifts = GiftHandle.ReadGifts(ctx.Guild);
+
+                if (gifts.Count < index && index - 1 < 0)
+                    await MessageHandle.SendError(ctx.Channel,
+                        message: $"index out of range (1-{gifts.Count})");
+                else
+                {
+                    gifts.RemoveAt(index - 1);
+
+                    File.Delete(PathHandle.GetGiftPath(ctx.Guild));
+                    GiftHandle.WriteGifts(ctx.Guild, gifts);
+
+                    await MessageHandle.SendSuccess(ctx.Channel,
+                        message: "gift removed from pool");
+                }
+            }
+
+            await message.DeleteAsync();
+        }
+
         [Command("gift-distribute")]
+        [Aliases("gd", "dist-g")]
         [Description("distribute gifts to all user and remove them from gift pool")]
         public async Task DistributeGift(CommandContext ctx)
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
-            var admins = new List<ulong>();
-            var path = PathHandle.GetAdminPath(ctx.Guild.Id);
-
-            var s = string.Empty;
-            using (var fs = File.Open(path, FileMode.OpenOrCreate))
-            using (var sr = new StreamReader(fs))
-                while ((s = sr.ReadLine()) != null)
-                    admins.Add(ulong.Parse(s));
-
-            if (!admins.Contains(ctx.User.Id))
-            {
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"error",
-                    Color = DiscordColor.Red,
-                    Description = $"{ctx.User.Username} is not admin"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
-            }
+            if (!RoleHandle.IsAdmin(ctx.Guild, ctx.User))
+                await MessageHandle.SendError(ctx.Channel,
+                    message: $"{ctx.User.Username} is not admin");
             else
             {
                 var random = new Random();
-                var gifts = new List<Tuple<ulong, string>>();
+                var gifts = GiftHandle.ReadGifts(ctx.Guild);
                 var used = new HashSet<int>();
                 var flip = false;
 
-                path = PathHandle.GetGiftPath(ctx.Guild.Id);
-
-                using (var fs = File.Open(path, FileMode.OpenOrCreate))
-                using (var sr = new StreamReader(fs))
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        var p = s.Split(seperator);
-                        gifts.Add(new Tuple<ulong, string>(ulong.Parse(p[0]), p[1]));
-                    }
-
                 if (gifts.Count == 0)
-                {
-                    var embed = new DiscordEmbedBuilder
-                    {
-                        Title = $"error",
-                        Color = DiscordColor.Red,
-                        Description = "no presents avaliable"
-                    };
-                    await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
-                }
+                    await MessageHandle.SendError(ctx.Channel,
+                        message: "no presents avaliable");
                 else
                 {
-                    var guild = ctx.Guild;
-
-                    foreach (var user in await guild.GetAllMembersAsync().ConfigureAwait(false))
+                    foreach (var user in await ctx.Guild.GetAllMembersAsync().ConfigureAwait(false))
                     {
                         if (used.Count == gifts.Count)
                         {
@@ -278,44 +261,33 @@ namespace Christmas_bot.Commands
                         while (used.Contains(i))
                             i = random.Next(0, gifts.Count);
 
-                        var sender = await guild.GetMemberAsync(gifts[i].Item1).ConfigureAwait(false);
+                        var sender = await ctx.Guild.GetMemberAsync(gifts[i].Item1).ConfigureAwait(false);
                         var gift = gifts[i].Item2;
 
-                        var embed = new DiscordEmbedBuilder
-                        {
-                            Title = $"presents",
-                            Color = DiscordColor.Green
-                        };
                         if (gift.StartsWith("text:"))
-                        {
-                            embed.Description = $"{user.Mention} got `{gift.Remove(0, 5)}` from {sender.Mention}";
-                        }
+                            await MessageHandle.SendSuccess(ctx.Channel,
+                                alternatetitle: "present",
+                                message: $"{user.Mention} got `{gift.Remove(0, 5)}` from {sender.Mention}");
                         else if (gift.StartsWith("url:"))
-                        {
-                            embed.Url = gift.Remove(0, 4);
-                            embed.Description = $"{user.Mention} got this from {sender.Mention}";
-                        }
+                            await MessageHandle.SendSuccess(ctx.Channel,
+                                alternatetitle: "present",
+                                message: $"{user.Mention} got this from {sender.Mention}",
+                                url: gift.Remove(0, 4));
                         else if (gift.StartsWith("image:"))
-                        {
-                            string actual = gift.Remove(0, 6);
-                            if (actual.EndsWith(".png") ||
-                                actual.EndsWith(".jpg"))
-                                embed.ImageUrl = actual;
-                            else
-                                embed.Url = actual;
-                            embed.Description = $"{user.Mention} got this from {sender.Mention}";
-                        }
-                        await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
+                            await MessageHandle.SendSuccess(ctx.Channel,
+                                alternatetitle: "present",
+                                message: $"{user.Mention} got this from {sender.Mention}",
+                                url: gift.Remove(0, 6),
+                                imageurl: gift.Remove(0, 6));
 
                         used.Add(i);
                     }
 
+                    var path = PathHandle.GetGiftPath(ctx.Guild);
+
                     File.Delete(path);
                     if (!flip)
-                        using (var sw = File.CreateText(path))
-                            for (int i = 0; i < gifts.Count; i++)
-                                if (!used.Contains(i))
-                                    sw.WriteLine($"{gifts[i].Item1}{seperator}{gifts[i].Item2}");
+                        GiftHandle.WriteGifts(ctx.Guild, gifts.Where((t, i) => !used.Contains(i)).ToList());
                 }
             }
 
@@ -328,40 +300,20 @@ namespace Christmas_bot.Commands
         {
             var message = await ctx.Channel.SendMessageAsync("processing...").ConfigureAwait(false);
 
-            var admins = new List<ulong>();
-            var path = PathHandle.GetAdminPath(ctx.Guild.Id);
-
-            var s = string.Empty;
-            using (var fs = File.Open(path, FileMode.OpenOrCreate))
-            using (var sr = new StreamReader(fs))
-                while ((s = sr.ReadLine()) != null)
-                    admins.Add(ulong.Parse(s));
-
-            if (!admins.Contains(ctx.User.Id))
-            {
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"error",
-                    Color = DiscordColor.Red,
-                    Description = $"{ctx.User.Username} is not admin"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
-            }
+            if (!RoleHandle.IsAdmin(ctx.Guild, ctx.User))
+                await MessageHandle.SendError(ctx.Channel,
+                    message: $"{ctx.User.Username} is not admin");
             else
             {
-                path = PathHandle.GetGiftPath(ctx.Guild.Id);
+                var path = PathHandle.GetGiftPath(ctx.Guild);
                 File.Delete(path);
 
-                var embed = new DiscordEmbedBuilder
-                {
-                    Title = $"success",
-                    Color = DiscordColor.Green,
-                    Description = $"{ctx.User.Username} cleared gifts pool"
-                };
-                await ctx.Channel.SendMessageAsync(embed).ConfigureAwait(false);
+                await MessageHandle.SendSuccess(ctx.Channel,
+                    message: $"{ctx.User.Username} cleared gifts pool");
             }
 
             await message.DeleteAsync();
         }
+
     }
 }
---
+++ Christmas bot/GiftHandle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace Christmas_bot
+{
+    internal static class GiftHandle
+    {
+        public static char seperator = '^';
+
+        public static void WriteGift(StreamWriter sw, ulong id, string gift) =>
+            sw.WriteLine($"{id}{seperator}{gift}");
+
+        public static void ReadGift(string s, out ulong id, out string gift)
+        {
+            var p = s.Split(seperator);
+            id = ulong.Parse(p[0]);
+            gift = p[1];
+        }
+
+        public static List<Tuple<ulong, string>> ReadGifts(DiscordGuild guild)
+        {
+            var gifts = new List<Tuple<ulong, string>>();
+
+            var s = string.Empty;
+            var path = PathHandle.GetGiftPath(guild);
+
+            using (var fs = File.Open(path, FileMode.OpenOrCreate))
+            using (var sr = new StreamReader(fs))
+                while ((s = sr.ReadLine()) != null)
+                {
+                    ReadGift(s, out ulong id, out string gift);
+                    gifts.Add(new Tuple<ulong, string>(id, gift));
+                }
+
+            return gifts;
+        }
+
+        public static void WriteGifts(DiscordGuild guild, List<Tuple<ulong, string>> gifts)
+        {
+            var path = PathHandle.GetGiftPath(guild);
+
+            if (gifts.Count > 0)
+                using (var sw = File.CreateText(path))
+                    for (int i = 0; i < gifts.Count; i++)
+                        WriteGift(sw, gifts[i].Item1, gifts[i].Item2);
+        }
+    }
+}
